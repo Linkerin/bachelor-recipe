c46977a4aa5cfcf5f7bf144a0de00b926548c821
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-06-05 15:51:54 +0300

    refactor: 📦️ Upgraded to SvelteKit v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-netlify';
 
