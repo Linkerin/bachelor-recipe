b6213e830e0348ef94840ed00c35f483d4a6d950
Linkerin <130695447+Linkerin@users.noreply.github.com>
2024-06-06 23:02:40 +0300

    refactor: ♻️ Added PageMeta type into App namespace

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,15 @@
+import type { PageMeta } from '$lib/types';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    interface PageData {
+      pageMeta?: PageMeta;
+    }
+    // interface Platform {}
+  }
 }
 
-export {};
+export { PageData };
